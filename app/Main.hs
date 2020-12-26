8d1a71403987493f465c9bd96cae35165a6c6879
Tero Laxström <tlax@neonpeons.com>
2020-12-26 13:58:54 +0000

    Basic parsing with error handling

--- app/Main.hs
+++ app/Main.hs
@@ -4,15 +4,16 @@ import Protolude
 
 import Control.Concurrent.STM.TVar (TVar, newTVar, readTVar, writeTVar, modifyTVar)
 import Data.Array (Array, listArray, assocs)
-import Data.Function ((&), id)
+import Data.Function (id)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
 import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core
+import qualified Text.ParserCombinators.ReadP as Parser
 
-import SpaceSloths.Assets (loadAssets)
+import SpaceSloths.Assets (Assets(..), loadAssets)
 import SpaceSloths.GameMap (GameMap(..), Cell(..), charToCell)
 import SpaceSloths.PathFinding
 import SpaceSloths.Renderer (renderGameView)
@@ -45,25 +46,63 @@ assetFiles =
 
 canvasSize = 400
 
-commandPrompt = UI.input & set (attr "placeholder") "Command"
+commandPrompt = UI.input # set (attr "placeholder") "Command"
 gameView = do
   canvas <- UI.canvas
-    & set UI.height canvasSize
-    & set UI.width canvasSize
-    & set style [("border", "solid black 1px"), ("background", "#eee")]
+    # set UI.height canvasSize
+    # set UI.width canvasSize
+    # set style [("border", "solid black 1px"), ("background", "#eee")]
   return canvas
+
+data Command = Render deriving (Show, Eq)
+
+data GameStatics
+  = GameStatics
+  { _gameView :: Element
+  , _gameAssets :: Assets
+  }
+
+evalCommand (GameStatics v a) m cmd =
+  case cmd of
+    Render -> renderGameView v a m
+
+parseCommand input = do
+  case mres of
+    Just res -> Right res
+    Nothing -> Left $ "Failed to parse: " <> input
+  where
+    mres = fst <$> head parse
+    parse =
+      Parser.readP_to_S commands input
+    commands =
+      Parser.choice
+        [ render
+        ]
+    render = do
+      _ <- Parser.string "Render"
+      return Render
 
 setup :: Window -> UI ()
 setup rootWin = do
-  return rootWin & set UI.title "Space Sloths"
+  return rootWin # set UI.title "Space Sloths"
   cp <- commandPrompt
+  cpText <- UI.div #. "command-prompt-ok" # set text ""
   gv <- gameView
-  getBody rootWin & set children [ cp, gv ]
+  getBody rootWin # set children [ cp, cpText, gv ]
   assets <- loadAssets missFile assetFiles
+  let statics = GameStatics gv assets
   treat UI.sendValue cp $ \input -> do
-    case input of
-      "render" -> renderGameView gv assets gameMap
-      _ -> return ()
+    let cmd = parseCommand input
+    case cmd of
+      Right c -> do
+        evalCommand statics gameMap c
+        element cp # set UI.value ""
+        element cpText # set text ("Evaluated " <> show c)
+           # set UI.style [("border", "2px solid black")]
+      Left e ->
+        element cpText
+          # set UI.style [("border", "2px solid red")]
+          # set text e
 
 main :: IO ()
 main = do
